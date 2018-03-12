c9b7af9e945688709ba97c503f4da43cfa82c6fc
Mirek <miroslaw.mac85@gmail.com>
2018-03-12 14:07:43 +0100

    12.03

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,17 @@
 const webpack = require('webpack');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const imageminMozjpeg = require('imagemin-mozjpeg');
 
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: __dirname + "/dist",
-        filename: 'bundle.js'
-    },
+function getPlugins(_isProd) {
+    var plugins = [];
 
-    watch: true,
-
-    devtool: "source-map",
-
-
-    module: {
-        rules: [
-            {
-                test: /\.(s*)css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: [{
-                        loader: "css-loader",
-                        options: {
-                            minimize: true,
-                            sourceMap: true
-                        }
-                    }, {
-                        loader: "sass-loader"
-                    }]
-                })
-            }
-        ],
-
-    },
-
-
-    plugins: [
-        // new UglifyJSPlugin({
-        //     sourceMap: true
-        // }),
+    plugins.push(
 
         new ExtractTextPlugin({
             filename: 'bundle.css'
@@ -62,19 +29,93 @@ module.exports = {
                 baseDir: "./"
             },
             files: [
-                'css/*.css',
-                'src/*.js',
+                './css/*.css',
+                './src/*.js',
+                './img/*.*',
                 './*.html'
             ]
         }),
 
         new CopyWebpackPlugin([{
-            from: 'images/'
+            from: 'img/',
+            to: 'img'
         }]),
-    ]
 
+    );
+
+    if (_isProd) {
+        plugins.push(
+
+            new UglifyJSPlugin({
+                sourceMap: true
+            }),
+
+            new ImageminPlugin({
+                test: /\.(jpe?g|png|gif|svg)$/i,
+
+                optipng: {
+                    optimizationLevel: 3
+                },
+
+                pngquant: {
+                    quality: '60-75'
+                },
+
+                jpegtran: null,
+
+                plugins: [
+                    imageminMozjpeg({
+                        quality: 60,
+                        progressive: true
+                    })
+                ],
+            })
+        );
+    }
+
+    return plugins;
+}
+
+
+module.exports = function (env) {
+
+    const isProd = env.production === true;
+
+    return {
+
+        entry: './src/app.js',
+        output: {
+            path: __dirname + "/dist",
+            filename: 'bundle.js'
+        },
+
+        watch: true,
+
+        devtool: "source-map",
+
+        module: {
+            rules: [
+                {
+                    test: /\.(s*)css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [{
+                            loader: "css-loader",
+                            options: {
+                                url: false,
+                                minimize: isProd,
+                                sourceMap: true
+                            }
+                        }, {
+                            loader: "sass-loader"
+                        }]
+                    })
+                }
+            ],
+
+        },
+
+        plugins: getPlugins(isProd)
+
+    }
 };
-
-
-
-
